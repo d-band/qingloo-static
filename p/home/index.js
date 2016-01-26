ec5608d5f64481dd3dd976d1ac44970496df21fc
振震 <865702942@qq.com>
2016-01-26 22:13:00 +0800

    home less changed

--- p/home/index.js
+++ p/home/index.js
@@ -1,9 +1,35 @@
+import hotTpl from './mods/hot.atpl';
+
 const main = {
-  init: function(){
-    console.log('Home page');
+  init: function() {
+    $('#J_hotList').html(hotTpl({
+      list: [{
+        name: '高血压吧',
+        description: '高血压（hypertension）是指以体循环动脉血压指以体循',
+        avatar: 'http://tb1.bdstatic.com/tb/%E5%85%AD%E5%B0%8F%E9%BE%84%E7%AB%A5.jpg'
+      }, {
+        name: '高血压吧',
+        description: '高血压（hypertension）是指以体循环动脉血压指以体循'
+      }, {
+        name: '高血压吧',
+        description: '高血压（hypertension）是指以体循环动脉血压指以体循',
+        avatar: 'http://tb1.bdstatic.com/tb/%E5%85%AD%E5%B0%8F%E9%BE%84%E7%AB%A5.jpg'
+      }, {
+        name: '高血压吧',
+        description: '高血压（hypertension）是指以体循环动脉血压指以体循'
+      }, {
+        name: '高血压吧',
+        description: '高血压（hypertension）是指以体循环动脉血压指以体循',
+        avatar: 'http://tb1.bdstatic.com/tb/%E5%85%AD%E5%B0%8F%E9%BE%84%E7%AB%A5.jpg'
+      }, {
+        name: '高血压吧',
+        description: '高血压（hypertension）是指以体循环动脉血压指以体循',
+        avatar: 'http://tb1.bdstatic.com/tb/%E5%85%AD%E5%B0%8F%E9%BE%84%E7%AB%A5.jpg'
+      }]
+    }));
   }
 };
 
-$(function(){
+$(function() {
   main.init();
 });
