5b261bb88cb0f9a768577ae318334846bf028f3d
振震 <865702942@qq.com>
2016-01-28 17:51:36 +0800

    add qiniu cdn gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp');
 var del = require('del');
+var qiniu = require('gulp-qiniu');
+var prompt = require('gulp-prompt');
 var uglify = require('gulp-uglify');
 var shell = require('gulp-shell');
 var pkg = require('./package.json');
+var join = require('path').join;
 
 gulp.task('clean', function(cb) {
   del(['dist/**'], cb);
@@ -30,3 +33,24 @@ gulp.task('lib-js', ['copy'], function() {
 });
 
 gulp.task('default', ['lib-js']);
+
+gulp.task('publish', ['lib-js'], function() {
+  gulp.src('package.json').pipe(prompt.prompt([{
+    type: 'input',
+    name: 'accessKey',
+    message: 'Please enter accessKey'
+  }, {
+    type: 'input',
+    name: 'secretKey',
+    message: 'Please enter secretKey'
+  }, {
+    type: 'input',
+    name: 'bucket',
+    message: 'Please enter bucket'
+  }], function(res) {
+    res.private = false;
+    return gulp.src('dist/**').pipe(qiniu(res, {
+      dir: join('/', pkg.name, pkg.version)
+    }));
+  }));
+});
