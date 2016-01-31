a2fc997c674941f97387d156f28898c1e9c149aa
振震 <865702942@qq.com>
2016-01-31 22:59:00 +0800

    add login and register page

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 require('bootstrap');
 require('./c/util');
 require('./c/request');
+
+var $login = $('#J_loginModal');
+$('#J_loginBtn').on('click', function() {
+  $login.modal('show').on('shown.bs.modal', function() {
+    $('.J_loginForm input[name=username]').focus();
+  });
+});
+$('#J_registerBtn').on('click', function() {
+  $login.modal('show').on('shown.bs.modal', function() {
+    $('.J_registerForm input[name=username]').focus();
+  });
+});
